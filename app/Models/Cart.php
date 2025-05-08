a2050cffd7584fe19129c4741010236a82d686d7
NhatLong213 <140641044+NhatLong213@users.noreply.github.com>
2025-05-08 12:59:59 +0700

    Create Cart.php

---
+++ app/Models/Cart.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'CartId';
+    protected $keyType = 'string'; 
+
+    protected $fillable = [
+        'CartId', 'CustomerId'
+    ];
+
+    function getCustomerId() {
+        return $this->belongsTo(Customer::class,'CustomerId','CustomerId');
+    }
+}
