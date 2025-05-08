fdf5ccf58d11dc3f09f085a38c086736a571b9fc
NhatLong213 <140641044+NhatLong213@users.noreply.github.com>
2025-05-08 12:59:51 +0700

    Create Bill.php

---
+++ app/Models/Bill.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Bill extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'BillId';
+    protected $keyType = 'string'; 
+
+    protected $fillable = [
+        'BillId', 
+        'CustomerId',
+        'created_at',
+        'Address',
+        'Phone',
+        'Status',
+        'Note',
+    ];
+}
