5cfbe6db6f507977ad4ea0f776bc8afab70e4e25
NhatLong213 <140641044+NhatLong213@users.noreply.github.com>
2025-05-08 13:00:09 +0700

    Create Customer.php

---
+++ app/Models/Customer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'CustomerId';
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'CustomerId',
+        'LastName',
+        'FirstName',
+        'Email',
+        'Gender',
+        'YearOfBirth',
+        'Phone',
+        'Address',
+    ];
+}
