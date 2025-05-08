5e38a52f5b09903dadc49a428ffd3c7ad603c390
NhatLong213 <140641044+NhatLong213@users.noreply.github.com>
2025-05-08 13:00:18 +0700

    Create Product.php

---
+++ app/Models/Product.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'ProductID';
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'ProductId',
+        'ProductName',
+        'ProductModelId',
+        'Ram',
+        'Memory',
+        'ShowOnHomePage',
+        'ProductThumbnail',
+        'UnitPrice',
+        'DesignSizeAndWeight',
+        'Warranty',
+        'Inventory',
+        'isNew',
+        'MonitorTechnology',
+        'Resolution',
+        'MonitorSize',
+        'CameraBack',
+        'CameraFront',
+        'Os',
+        'Cpu',
+        'CpuSpeed',
+        'Gpu',
+        'Cellular',
+        'Sim',
+        'Wireless',
+        'Port',
+        'Jack',
+        'BatteryCapacity',
+        'BatteryType', 
+        'BatteryTechnology',
+        'NumberOfCore',
+        'NumberOfThread',
+        'SpecialFeature',
+        'ChargerIncluded',
+        'MaximumChargable',
+        'MaximumRamUpgraded',
+        'MaximumCpuSpeed',
+        'StrapReplaceable',
+        'SportSupport',
+        'CallingSupport',
+        'MonitorMaterials',
+        'BorderMaterials',
+        'StrapMaterials',
+        'StrapWidth',
+        'StrapHeight',
+        'WaterResistant',
+        'Healthcare',
+        'DisplayNotification',
+        'ChargingTime',
+        'OsConnectable',
+        'ManagermentApplication',
+        'Sensor',
+        'Locater'
+    ];
+    protected static function boot(){
+        parent::boot();
+        static::creating(function ($product) {
+            $lastProduct = Product::orderBy('ProductId', 'desc')->first();
+            if($lastProduct){
+                $lastNumber = (int)substr($lastProduct->ProductId,3) + 1;
+            }
+            else $lastNumber = 1;
+            $product->ProductId = "P" . str_pad($lastNumber,9,"0",STR_PAD_LEFT); 
+        });
+    }
+
+    function getProductModelName() {
+        return $this->belongsTo(ProductModel::class,'ProductModelId','ProductModelId');
+    }
+}
