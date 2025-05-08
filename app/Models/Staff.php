a30b9ecd84c91dee605b058a2d6e6014ede58a0e
NhatLong213 <140641044+NhatLong213@users.noreply.github.com>
2025-05-08 13:00:37 +0700

    Create Staff.php

---
+++ app/Models/Staff.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Staff extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'StaffId';
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'StaffId',
+        'StaffPositionId',
+        'StaffName',
+        'Email',
+        'Gender',
+        'YearOfBirth',
+        'Phone',
+        'CitizenId',
+    ];
+
+    protected static function boot(){
+        parent::boot();
+        static::creating(function ($staff) {
+            $lastStaff = Staff::orderBy('StaffId', 'desc')->first();
+            if($lastStaff){
+                $lastNumber = (int)substr($lastStaff->StaffId,3) + 1;
+            }
+            else $lastNumber = 1;
+            $staff->StaffId = "S" . str_pad($lastNumber,9,"0",STR_PAD_LEFT); 
+        });
+    }
+
+    function getStaffPositionName() {
+        return $this->belongsTo(StaffPosition::class,'StaffPositionId','StaffPositionId');
+    }
+}
