e7eaf25fda34209d68a63ac2d14535f6aa23fa3b
NhatLong213 <140641044+NhatLong213@users.noreply.github.com>
2025-05-08 13:08:57 +0700

    Create 2024_04_08_095525_create_product_models_table.php

---
+++ database/migrations/2024_04_08_095525_create_product_models_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_models', function (Blueprint $table) {
+            $table->integer('ProductModelId')->primary();
+            $table->string('ProductModelName', 50)->nullable(false);
+            $table->enum('ProductTypeId', [1,2,3,4]);
+            
+            $table->foreign('ProductTypeId')->references('ProductTypeId')->on('product_types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_models');
+    }
+};
